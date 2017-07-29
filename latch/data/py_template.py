ceebd8a225bbeb888b18e8ecb415113441515a11
Noah Weinthal <nweinthal@gmail.com>
2017-07-29 01:54:11 -0400

    Update py template with CORS functionality on routes

--- latch/data/py_template.py
+++ latch/data/py_template.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 import redis
 import urllib.parse
+import aiohttp_cors
 
 from aiohttp import web
 from collections import namedtuple
@@ -13,8 +14,12 @@ db = redis.StrictRedis('localhost', 6379)
 
 {% for definition in parsed.definitions %}
 async def {{['list', definition, 'Handler']|join('')}}(request):
-  {{defintion|lower}}_set = db.smembers('{{definition}}')
-  decoded = set(MessageToDict(i) for i in {{definition|lower}}_set)
+  {{definition|lower}}_set = db.smembers('{{definition}}')
+  decoded = []
+  for bytestring in {{definition|lower}}_set:
+    msg = api.{{definition}}()
+    msg.ParseFromString(bytestring)
+    decoded.append(MessageToDict(msg))
   return web.Response(text=json.dumps(decoded))
 
 async def {{['get', definition, 'Handler']|join('')}}(request):
@@ -23,7 +28,7 @@ async def {{['get', definition, 'Handler']|join('')}}(request):
 async def {{['create', definition, 'Handler']|join('')}}(request):
   data = await request.json()
   item = api.{{definition}}(**data)
-  db.sadd('{{definition}}', item)
+  db.sadd('{{definition}}', item.SerializeToString())
   return web.json_response(MessageToJson(item))
 
 async def {{['update', definition, 'Handler']|join('')}}(request):
@@ -31,10 +36,20 @@ async def {{['update', definition, 'Handler']|join('')}}(request):
 
 async def {{['delete', definition, 'Handler']|join('')}}(request):
   return web.Response(text="Hello, world")
+
+async def {{['options', definition, 'Handler']|join('')}}(request):
+  return web.Response(status=204)
 {% endfor %}
 
 if __name__ == '__main__':
   app = web.Application()
+  cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+       allow_credentials=True,
+       allow_methods="*",
+       expose_headers="*",
+       allow_headers="*")
+  })
   {% for definition in parsed.definitions %}
   resource_root = urllib.parse.urljoin('{{parsed.basePath}}/',
       '{{definition|lower}}s')
@@ -43,32 +58,45 @@ if __name__ == '__main__':
   {{definition|lower}}_create_url = resource_root
   {{definition|lower}}_delete_url = resource_root + '/{id}'
   {{definition|lower}}_update_url = resource_root + '/{id}'
-
+  {{definition|lower}}_options_url = resource_root
   # LIST
-  list_resource = app.router.add_resource({{definition|lower}}_list_url,
-      name='List{{definition}}s')
-  list_route = list_resource.add_route('GET', list{{definition}}Handler)
+  list_resource = cors.add(
+    app.router.add_resource({{definition|lower}}_list_url,
+      name='List{{definition}}s'))
+  cors.add(list_resource.add_route('GET', list{{definition}}Handler))
 
   # GET
-  get_resource = app.router.add_resource({{definition|lower}}_get_url,
-      name='Get{{definition}}')
-  get_route = get_resource.add_route('GET', get{{definition}}Handler)
+  get_resource = cors.add(
+    app.router.add_resource({{definition|lower}}_get_url,
+      name='Get{{definition}}'))
+  cors.add(get_resource.add_route('GET', get{{definition}}Handler))
 
   # CREATE
-  create_resource = app.router.add_resource({{definition|lower}}_create_url,
-      name='Create{{definition}}')
-  create_route = create_resource.add_route('POST', create{{definition}}Handler)
+  create_resource = cors.add(
+    app.router.add_resource({{definition|lower}}_create_url,
+      name='Create{{definition}}'))
+  cors.add(create_resource.add_route('POST', create{{definition}}Handler))
 
   # UPDATE
-  update_resource = app.router.add_resource({{definition|lower}}_update_url,
-      name='Update{{definition}}')
-  update_route = update_resource.add_route('PATCH',
-      update{{definition}}Handler)
+  update_resource = cors.add(
+    app.router.add_resource({{definition|lower}}_update_url,
+      name='Update{{definition}}'))
+  cors.add(update_resource.add_route('PATCH',
+      update{{definition}}Handler))
 
   # DELETE
-  delete_resource = app.router.add_resource({{definition|lower}}_delete_url,
-      name='Delete{{definition}}')
-  delete_route = delete_resource.add_route('DELETE',
-      delete{{definition}}Handler)
+  delete_resource = cors.add(
+    app.router.add_resource({{definition|lower}}_delete_url,
+      name='Delete{{definition}}'))
+  cors.add(delete_resource.add_route('DELETE',
+      delete{{definition}}Handler))
+
+  ##OPTIONS
+  #options_resource = cors.add(
+  #    app.router.add_resource({{definition|lower}}_options_url,
+  #    name='Options{{definition}}'))
+  #cors.add(options_resource.add_route('OPTIONS',
+  #    options{{definition}}Handler))
+
   {% endfor %}
   web.run_app(app, port=8888)
